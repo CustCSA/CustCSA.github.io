9f44aa8983f5ac3162deeb28b6dc24d03c826262
Yohane-Mashiro <Yohane-Mashiro@users.noreply.github.com>
2025-01-11 02:28:42 +0000

    Updated By Github Actions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1735983017067";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1736562520046";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
